256338c225e7e361ba8ca926434417ea9927df27
Sishir <smollet3@gitam.in>
2026-01-21 21:52:45 +0530

    add

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true, trim: true },
-    password: { type: String, required: true }, // hashed
+    password: { type: String, required: true },
   },
   { timestamps: true }
 );
